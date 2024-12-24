8953008be1b8098e9624475b4bb9311384793926
Golu Kumar <golug73700@gmail.com>
2024-12-24 15:53:44 +0530

    first commit

---
+++ Components/src/App.jsx
@@ -0,0 +1,62 @@
+export const App = () => {
+  return ( 
+    <div>
+     
+    <div>
+      <div>
+      <img src="/image1.jpg" alt="Queen of Tears" height="40%" width={"40%"} />
+
+      </div>
+      <h2>Name : Queen of Tears </h2>
+      <h3>Rating:8.2</h3>
+      <p>
+        Summry : Baek Hyun-woo Hong-in navigate a tense relationship,
+        both at home and at work. But upon deciding his future, Hyun-woo pays a Hyun-woo
+        pays a visit to his family.
+      </p>
+    </div>
+    
+
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries></NetflixSeries > 
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries/>
+
+
+    </div>
+// you can write <NetflixSeries></NetflixSeries >  or <NetflixSeries/> 
+  );
+};
+
+
+const NetflixSeries = () => {
+  return(
+    <div>
+    <div>
+    <img src="/image1.jpg" alt="Queen of Tears" height="40%" width={"40%"} />
+
+    </div>
+    <h2>Name : Queen of Tears </h2>
+    <h3>Rating:8.2</h3>
+    <p>
+      Summry : Baek Hyun-woo Hong-in navigate a tense relationship,
+      both at home and at work. But upon deciding his future, Hyun-woo pays a Hyun-woo
+      pays a visit to his family.
+    </p>
+  </div>
+  )
+}
