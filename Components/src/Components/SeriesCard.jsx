ceeb884c8d5d44b6a625b6dbbb15c61bad79af10
Golu Kumar <golug73700@gmail.com>
2024-12-27 15:20:29 +0530

    Add" inline "  css

--- Components/src/Components/SeriesCard.jsx
+++ Components/src/Components/SeriesCard.jsx
@@ -3,32 +3,57 @@ export const SeriesCard = ({data}) => {
    // console.log(props);
   // eslint-disable-next-line react/prop-types
   const {  img_url, name, rating, description, cast, genre, watch_url} = data;
+  
+  const btn_style = {
+
+    padding:"1.2rem 2.4rem",
+      border: "none",
+      fontSize: "1.6rem",
+      backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+      color : "var( --btn-color)",
+      fontweight: "bold",
+      cursor : "pointer",
+
+      
+
+  };
+
+const ratingClass = rating >= 8.5 ? "super_hit" : "average";
+
+
+  
   return (
 
-          <li>
+          <li className="card">
           
             {/* Image */}
             <div>
-              <img
-                src={img_url}
-                alt={name}
-                width="60%"
-                height="60%"
-                style={{ objectFit: "cover" }}
-              />
+              <img  src={img_url} alt={name} width="40%" height="40%" style={{ objectFit: "cover" }}  />
             </div>
-  
+            <div className="card-content">
+
             {/* Series Details */}
             <h2>Name: {name}</h2>
-            <h3>Rating: {rating}</h3>
+            <h3>
+              Rating:
+               <span className={`rating ${ratingClass}`}> {rating} </span>
+               
+
+                 </h3>
+
             <p>Summary: {description}</p>
             <p>Genre: {genre}</p>
             <p>Cast: {cast}</p>
   
             {/* Watch Button */}
             <a href={watch_url} target="_blank" rel="noopener noreferrer">
-              <button>Watch Now</button>
+             
+              <button style={btn_style}> Watch Now</button>
+               
+            
             </a>
+            
+            </div>
           </li>
 
     );
