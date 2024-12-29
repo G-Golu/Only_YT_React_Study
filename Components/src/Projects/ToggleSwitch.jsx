37220fd4040ea74801b8acc7ba2f7edbb324f4c1
Golu Kumar <golug73700@gmail.com>
2024-12-29 14:28:48 +0530

    Add Icon in ToggleSwitch

--- Components/src/Projects/ToggleSwitch.jsx
+++ Components/src/Projects/ToggleSwitch.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import "./ToggleSwitch.css";
 import { IoIosSwitch } from "react-icons/io";
 import { PiUserSwitchFill } from "react-icons/pi";
-
+import { CiLock } from "react-icons/ci";
+import { TfiApple } from "react-icons/tfi";
 export const ToggleSwitch = () => {
   const [isOn, setIsOn] = useState(false);
 
@@ -11,16 +12,23 @@ export const ToggleSwitch = () => {
   };
 
   const checkIsOn = isOn ? "on" : "off";
-  const toggleBGColor = { backgroundColor: isOn ? "#4caf50" : "" };
+  const toggleBGColor = { backgroundColor: isOn ? "#4caf50" : "#f44336" };
 
   //subscribe to Thapa Technical Youtube Channel - https://www.youtube.com/thapatechnical
   return (
     <>
+    
+
+    {/** this is h1 tag all element cant take online serh react icon and past "import ..."  and past this line ..*/}
       <h1 style={{ color: "#000", textAlign: "center" }}>
         Toggle Switch{" "}
         <IoIosSwitch style={{ color: "red", textAlign: "center" }} />
-        <PiUserSwitchFill />
-      </h1>
+        <PiUserSwitchFill style={{ color: "black", textAlign: "center" }} />
+        
+        <CiLock style={{ color: "green", textAlign: "center" }}/>
+      </h1><TfiApple style={{ color: "green", textAlign: "center"  }} />  
+
+      
       <div
         className="toggle-switch"
         style={toggleBGColor}
